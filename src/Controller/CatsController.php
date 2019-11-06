eddab9c124df99075fe3887c0573b4b529c1dabf
valentinmollet <valentinmollet@gmail.com>
2019-11-06 10:15:15 +0100

    feat: form without image

---
+++ src/Controller/CatsController.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Cat;
+use App\Form\CatType;
+use App\Repository\CatRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/cats", name="cats_")
+ */
+class CatsController extends AbstractController
+{
+    /**
+     * @var CatRepository
+     */
+    private $repository;
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    public function __construct(CatRepository $catRepository, ObjectManager $em){
+        $this->repository = $catRepository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/", name="list")
+     * @return Response
+     */
+    public function list() {
+        $cats = $this->repository->findAll();
+        return $this->render('base.html.twig', [
+            'cats' => $cats
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     * @param Request $request
+     * @return Response
+     */
+    public function create(Request $request) {
+        $cat = new Cat();
+        $form = $this->createForm(CatType::class, $cat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($cat);
+            $this->em->flush();
+            return $this->redirectToRoute('cats_list');
+        }
+
+        return $this->render('create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Entity/Cat.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CatRepository")
+ */
+class Cat
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $race;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $owner;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getRace(): ?string
+    {
+        return $this->race;
+    }
+
+    public function setRace(string $race): self
+    {
+        $this->race = $race;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getOwner(): ?string
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(string $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+}
