54f2913bdc46c6c97063cc69c7ce8e61e1ed87c3
valentinmollet <valentinmollet@gmail.com>
2019-11-08 19:28:51 +0100

    feat: auth gestion

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
